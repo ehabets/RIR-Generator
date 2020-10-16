c3345f9694289e6cd4b79c1dd4aa97f41f27e89f
Emanuël Habets <e.habets@ieee.org>
2020-10-16 21:57:55 +0200

    Update rir_generator.cpp
    Updated license

--- rir_generator.cpp
+++ rir_generator.cpp
@@ -13,7 +13,7 @@ Description : Computes the response of an acoustic source to one or more
               acoustic source in a reverberant room, Journal Acoustic
               Society of America, 80(5), November 1986.
  
-Author      : dr.ir. E.A.P. Habets (ehabets@dereverberation.org)
+Author      : dr.ir. E.A.P. Habets (e.habets@ieee.org)
  
 Version     : 2.1.20141124
  
@@ -38,21 +38,27 @@ History     : 1.0.20030606 Initial version
               2.1.20141124 + The window and sinc are now both centered
                              around t=0
  
-Copyright (C) 2003-2014 E.A.P. Habets, The Netherlands.
+MIT License
+
+Copyright (C) 2003-2014 E.A.P. Habets
  
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
- 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
- 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 */
 
 #define _USE_MATH_DEFINES
@@ -133,7 +139,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
             "|                                                                  |\n"
             "| Version   : 2.1.20141124                                         |\n"
             "|                                                                  |\n"
-            "| Copyright (C) 2003-2014 E.A.P. Habets, The Netherlands.          |\n"
+            "| Copyright (C) 2003-2014 E.A.P. Habets                            |\n"
             "|                                                                  |\n"
             "| [1] J.B. Allen and D.A. Berkley,                                 |\n"
             "|     Image method for efficiently simulating small-room acoustics,|\n"
@@ -177,7 +183,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     else
     {
         mexPrintf("Room Impulse Response Generator (Version 2.1.20141124) by Emanuel Habets\n"
-            "Copyright (C) 2003-2014 E.A.P. Habets, The Netherlands.\n");
+            "Copyright (C) 2003-2014 E.A.P. Habets\n");
     }
 
     // Check for proper number of arguments
