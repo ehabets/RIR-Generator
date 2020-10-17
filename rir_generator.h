d5f72c3e1fec21784ee19cb5f5a502370bd6196c
Nils Werner <nils@hey.com>
2020-10-17 09:37:20 +0200

    Split MEX and non-MEX code into separate files

---
+++ rir_generator.h
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+void computeRIR(double* imp, double c, double fs, double* rr, int nr_of_mics, int nsamples, double* ss, double* LL, double* beta, char* mtype, int order, double* angle, int hp_filter);
